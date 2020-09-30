2f4d3e3613ddd158a7bd026d12e8c49266e5e048
darcy <steel.princess@gmail.com>
2020-09-30 02:16:21 -0700

    Add very basic data manager and some stub data

---
+++ src/dataManager.js
@@ -0,0 +1,33 @@
+// TODO: make this read from CSV and at least pretend to be performant (not loading everything at once)
+
+const items = require('./data/items.json');
+const keys = require('./data/keys.json');
+const rooms = require('./data/rooms.json');
+const strings = require('./data/strings.json');
+
+function getString (id) {
+    return strings[id];
+}
+function getItem (id) {
+    return items[id];
+}
+function getKey (id) {
+    return keys[id];
+}
+function getRoom (id) {
+    return hydrateRoom(rooms[id]);
+}
+
+function hydrateRoom (room) {
+    return {
+        ...room,
+        desc: getString(room.desc_id)
+    }
+}
+
+module.exports = {
+    getString,
+    getItem,
+    getKey,
+    getRoom
+}
